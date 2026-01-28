87500bddd43c1b8d9f169ab15d70a851de21ea3d
RamOOmaR93 <raimonds.sabans@inbox.lv>
2026-01-29 00:01:45 +0200

    Uzsakts projekts

--- backend/src/test/java/lv/pmeu/pmeu_sistema/PmeuUzskaitesSistemaApplicationTests.java
+++ backend/src/test/java/lv/pmeu/pmeu_sistema/PmeuUzskaitesSistemaApplicationTests.java
@@ -1,13 +1,10 @@
 package lv.pmeu.pmeu_sistema;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PmeuUzskaitesSistemaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	
 
 }
