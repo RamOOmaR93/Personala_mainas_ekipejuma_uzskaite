eaf568d7cdc895ac8f8dd706cd53b7d53f6068ab
RamOOmaR93 <raimonds.sabans@inbox.lv>
2026-01-28 23:08:50 +0200

    Izveidots backend un frontend

---
+++ backend/src/main/java/lv/pmeu/pmeu_sistema/PmeuUzskaitesSistemaApplication.java
@@ -0,0 +1,13 @@
+package lv.pmeu.pmeu_sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmeuUzskaitesSistemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmeuUzskaitesSistemaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/lv/pmeu/pmeu_sistema/PmeuUzskaitesSistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.pmeu.pmeu_sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmeuUzskaitesSistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
